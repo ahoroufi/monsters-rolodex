2db11cbabd2f9cb6d60ec403c36034fa507b11b9
Arash Horoufi <6829077+ahoroufi@users.noreply.github.com>
2025-10-01 15:56:12 -0700

    Folder Structure and Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import {useEffect, useState} from 'react';
+
+import CardList from './components/card-list/card-list.component';
+
 import './App.css';
 
-const Card = ({name, email}) => {
-  return (
-    <div>
-      <h3>{name}</h3>
-      <p>{email}</p>
-    </div>
-  )
-}
+
 
 const App = () => {
   const [monsters, setMonsters] = useState([]); // twople
-  console.log(monsters)
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -22,10 +17,7 @@ const App = () => {
 
   return (
     <>
-      {monsters.map(({name, email}, idx) => {
-          // <Card name='a'></Card>
-          return <Card key={`${name}-${idx}`} name={name} email={email}></Card>
-      })}
+      <CardList monsters={monsters} />
     </>
   ); 
 };
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,17 @@
+import Card from '../card/card.component';
+
+import './card-list.styles.css';
+
+const CardList = ({ monsters }) => {
+    return (
+        <div className='card-list-container'>
+            {monsters.map(({name, email, id}, idx) => {
+            return (
+                <Card key={`${name}-${idx}`} name={name} email={email} id={id}></Card>
+            );
+            })}
+        </div>
+    );
+};
+
+export default CardList;
